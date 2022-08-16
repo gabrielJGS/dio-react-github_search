b2c4a3d42dac46b6430281a3b1eb87f00bbc734d
gabrielJGS <gabrielftjose@gmail.com>
2022-08-15 21:22:40 -0300

    feat: no user selected component

---
+++ src/components/no-search/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as s from "./styled";
+
+const NoSearch = () => {
+  return (
+    <s.Wrapper>
+      <h1>Nenhum usuário pesquisado</h1>
+    </s.Wrapper>
+  );
+};
+
+export default NoSearch;
